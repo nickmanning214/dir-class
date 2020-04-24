70318c8f17f72c38f6f01eef222c7abb77b4a97c
Nick Manning <nickmanning214@gmail.com>
2020-04-24 10:15:37 -0400

    commit

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
 const fs = require('fs-extra');
+const walkDirectory = require('@nickmanning214/walk-tree/implementations/walkDirectory.js')
+const path = require('path');
+const difference = require('lodash.difference');
+const intersection = require('lodash.intersection');
 
 class Directory{
-    constructor(path){
-        this.path = path;
+    constructor(pathString){
+        this.path = pathString;
+
+
+        this.parentDir = path.parse(pathString).dir;
+        this.name = path.parse(pathString).name;
+        //this.children = this.walk()
+
+
         this.liftTempDestination = `/var/tmp/ahu8siornau4hrivfuoan`
     }
     lift(){
@@ -26,6 +37,47 @@ class Directory{
             });
         })
     }
+
+    //todo: walk returns nodes, paths returns full paths, and another function returns relative paths.
+
+    walk(map=true){
+        const parsed = path.parse(this.path);
+        const nodes =  walkDirectory(parsed.dir,parsed.name);
+        if (!map) return nodes;
+        else return nodes.map(n=>`${n.metaData.parentPath}/${n.value}`);
+    }
+
+    relativePaths(){
+        return this.walk().map(path=>path.substr(this.parentDir.length+1+this.name.length));
+    }
+
+    compare(path){
+        const thesePaths = this.relativePaths();
+        const compareTo = new Directory(path);
+        const comparePaths = compareTo.relativePaths();
+
+
+        const fileNamesInBoth = intersection(thesePaths,comparePaths);
+        const differingContents = [];
+        fileNamesInBoth.forEach(fileName=>{
+            const path1 = `${this.path}${fileName}`;
+            const path2 = `${path}${fileName}`;
+            if ([path1,path2].every(e=>fs.lstatSync(e).isFile()) && fs.readFileSync(path1).toString()!==fs.readFileSync(path2).toString()) differingContents.push(fileName);
+        })
+        
+
+        const ret = {
+            fileNamesOnlyFirst:difference(thesePaths,comparePaths),
+            fileNamesOnlySecond:difference(comparePaths,thesePaths),
+            differingContents
+        };
+
+
+
+        return ret;
+
+    }
+
 
 }
 
--- test/test.js
+++ test/test.js
@@ -16,7 +16,7 @@ describe('Directory',()=>{
     });
 });
 
-describe('Directory.lift/drop',()=>{
+describe('Directory#lift/drop',()=>{
 
     const dirpath = path.join(__dirname,DIR_NAME);
 
@@ -30,11 +30,59 @@ describe('Directory.lift/drop',()=>{
     });
 
     it('should lift the directory and drop it',async ()=>{
+        before(()=>{
+            console.log("Hello")
+        })
         assert(fs.existsSync(dirpath))
         await dir.lift();
         assert(!fs.existsSync(dirpath))
         await dir.drop();
         assert(fs.existsSync(dirpath))
+
+    })
+});
+
+describe('Directory#walk',()=>{
+    before(()=>{
+        dir = new Directory(path.join(__dirname,'dummy-directory'))
+    })
+    it('should walk the directory',()=>{
+        const paths = dir.walk().sort().join(' ');
+        const compared = [
+            'dummy-directory',
+            'dummy-directory/depth-1',
+            'dummy-directory/depth-2', 
+            'dummy-directory/depth-1/file1.txt', 
+            'dummy-directory/depth-1/file2.txt', 
+            'dummy-directory/depth-2/file3.txt', 
+            'dummy-directory/depth-2/file4.txt'].map(p=>path.join(__dirname,p)).sort().join(' ');
+
+        assert(paths == compared);
+
+
+    });
+    it('should return the nodes and not the string paths if you pass false',()=>{
+        const nodes = dir.walk(false);
+        assert(typeof nodes[0] == 'object' && nodes[0].metaData && nodes[0].value);
+    })
+});
+
+describe('Directory#compare',()=>{
+    before(()=>{
+        dir = new Directory(path.join(__dirname,'dummy-compare-1'))
+    });
+    it('should compare the directory with another',()=>{
+        const compared = dir.compare(path.join(__dirname,'dummy-compare-2'))
+        assert.deepEqual(compared.fileNamesOnlyFirst,[ '/dir-in-1',
+            '/dir-in-1/file1.txt',
+            '/dir-in-1/file2.txt',
+            '/dir-in-both/file-in-1.txt' ])
+        assert.deepEqual(compared.fileNamesOnlySecond,[ '/dir-in-2',
+        '/dir-in-2/file1.txt',
+        '/dir-in-2/file2.txt',
+        '/dir-in-both/file-in-2.txt' ])
+
+        assert.deepEqual(compared.differingContents,[ '/dir-in-both/file-in-both-diff-contents.txt' ] )
 
     })
 })
